86215e5b16ad5f8dccc30674412e9b11134168c3
Matt Helgen <mdhelgen@gmail.com>
2011-09-19 18:38:13 -0400

    open() error checks

--- proj1-fritter/fritter.c
+++ proj1-fritter/fritter.c
@@ -45,8 +45,14 @@ int main(int argc, char** argv){
 
 	//did the open fail?
 	if(fdAcl == -1){
-		perror("aclFile open()");
-		exit(1);
+		if(errno == ENOENT){
+			printf("Error: %s does not exist\n", aclFileName);
+			exit(1);
+		}
+		else{
+			perror("aclFile open()");
+			exit(1);
+		}
 	}
 
 	a = 0;
@@ -62,8 +68,14 @@ int main(int argc, char** argv){
 
 	//did the open fail?
 	if(fdFile == -1){
-		perror("logfile open()");
-		exit(1);
+		if(errno == ENOENT){
+			printf("Error: %s does not exist\n", argv[1]);
+			exit(1);
+		}
+		else{	
+			perror("logfile open()");
+			exit(1);
+		}
 	}
 	
 
